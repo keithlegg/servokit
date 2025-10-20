ee79aa0c2b092ddce4351f7b8512ac0bf8aecfbb
keith <keithlegg23@gmail.com>
2025-10-20 09:18:40 -0700

    added 2560

---
+++ atmega2560/src/actuator.c
@@ -0,0 +1,207 @@
+#include <avr/io.h>
+
+#define F_CPU 16000000UL //AVR Clock Speed in MHZ
+#define FOSC 16000000    // Clock Speed
+
+//UART STUFF
+#define BAUD 57600
+#define MYUBRR FOSC/16/BAUD-1
+
+
+
+
+
+#define BIT_ON 0x30 //logic high
+#define BIT_OFF 0x31 //logic low
+
+#include <util/delay.h>
+
+
+/***********************************************/
+
+/*
+
+    (17 bit address bus) I wired 17 high to test 
+    --------------------------------
+    address (L)  (H)    | CONTROL 
+           PORTC PORTL  | PORTD
+           [0-7] [8-13] | [1-3]  
+    --------------------------------
+    PORTL - address bus low   (7 bit)
+    PORTC - address bus high  (7 bit)
+    PORTK - data bus
+    PORTD - control bus       (3-4 bits)
+
+
+*/
+
+
+
+
+/*
+
+    (15 bit address bus)
+    --------------------------------
+    address (L)  (H)    | CONTROL 
+           PORTC PORTL  | PORTD
+           [0-7] [8-13] | [1-3]  
+    --------------------------------
+    PORTL - address bus low   (7 bit)
+    PORTC - address bus high  (7 bit)
+    PORTK - data bus
+    PORTD - control bus       (3 bits)
+
+
+*/
+
+
+/***********************************************/
+/***********************************************/
+
+//PORTD - CONTROL BUS - bits 1-3
+#define WE_PIN 0x01 
+#define OE_PIN 0x02
+#define CS_PIN 0x04  //CE is CS 
+#define CS2_PIN 0x08  //CE is CS 
+
+/***********************************************/
+
+void USART_Init( unsigned int ubrr)
+{
+    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+}
+
+
+/***********************************************/
+
+static uint8_t USART_receive(void)
+{
+    while (!(UCSR0A & (1 << RXC0))) {}
+    return UDR0;
+}
+
+/***********************************************/
+
+void USART_Transmit( unsigned char data )
+{
+    while ( !( UCSR0A & (1<<UDRE0)) );
+    UDR0 = data;
+}
+
+
+/***********************************************/
+
+void print_byte( uint8_t data){
+   uint8_t i = 0;
+
+   for (i=0; i<=7; i++) {
+       //if ( !!(data & (1 << ii)) ){  // LSB
+       if ( !!(data & (1 << (7 - i))) ){  // MSB
+           USART_Transmit( BIT_OFF );
+       }else{
+           USART_Transmit( BIT_ON );
+       }
+    }
+    USART_Transmit( 0xa ); //CHAR_TERM = new line  
+    USART_Transmit( 0xd ); //0xd = carriage return
+}
+
+/***********************************************/
+uint8_t idx_to_byte(uint8_t idx){
+    return (1 << idx);
+} 
+
+
+void test_ports(void){
+    int a=0;
+    for(a=0;a<8;a++){
+       PORTL = idx_to_byte(a);
+       _delay_ms(100);
+    }
+    for(a=0;a<8;a++){
+       PORTC = idx_to_byte(a);
+       _delay_ms(100);
+    }       
+}
+
+
+
+/***********************************************/
+/*
+uint8_t read_ram(uint16_t address){
+    
+    uint8_t out = 0x00;
+
+    DDRK = 0x00; 
+    PORTD |= CS_PIN; 
+    PORTK = 0x00; //clear data port
+    set_address(address);
+
+    PORTD |= WE_PIN; //WE high when reading 
+    PORTD |= OE_PIN; 
+
+    PORTD &= ~CS_PIN; 
+    PORTD &= ~OE_PIN; 
+
+    out = PINK;
+    
+    PORTK = 0x00; //clear data port
+
+    PORTD |= OE_PIN; 
+    PORTD |= CS_PIN; 
+
+    return out;
+}
+
+void write_ram(uint16_t address, uint8_t byte){
+    
+    DDRK = 0xff;
+    PORTD |= CS_PIN;  
+    PORTD |= WE_PIN; 
+    PORTD |= OE_PIN;     
+
+    set_address(address);
+
+    PORTD &= ~CS_PIN;  
+
+    PORTK = byte;
+
+    PORTD &= ~WE_PIN;     
+    PORTD |= WE_PIN;  
+
+}
+*/
+
+/***********************************************/
+
+int main (void)
+{
+
+    DDRC = 0xff;     
+    DDRD = 0xff;  
+    DDRL = 0xff; 
+    //DDRK SET DYNAMICALLY 
+
+    PORTD |= 0x08; 
+    //DDRD |= (0x4); // PORTD!
+    
+    //USART_Init(MYUBRR);
+  
+    uint16_t a = 0;
+
+    while(1)
+    {   
+        //USART_Transmit(0x41); 
+        _delay_ms(100);
+    }
+
+
+} 
+
+/***********************************************/
+
+
+
---
+++ atmega328/src/examples/_pulse_two_button.c
@@ -0,0 +1,127 @@
+\
+#define F_CPU 16000000L // Define software reference clock for delay duration
+#define __DELAY_BACKWARD_COMPATIBLE__
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+/*******************************/
+
+/*
+
+        DEBUG!!! UNFINISHED !!!!
+
+
+
+
+
+   Simple tool to make some blinkys for a reeeeal simple motor controller 
+   just to hit the ground running and get moving 
+
+
+   Pinouts: 
+       DDRB |= (1 << 5);  //PB5 arduino led
+
+       sbi(DDRB, 3 );     //PB5 arduino pin 11
+       DDRB =0b00001000;  //PB3 arduino pin 11 
+       DDRB |= (1 << 3);  //PB3 arduino pin 11
+
+*/   
+
+
+
+/*******************************/
+
+
+
+#define sbi(a, b) (a) |= (1 << (b))
+#define cbi(a, b) (a) &= ~(1 << (b))
+
+
+#define MOTOR_ENABLE_PORT PORTB
+#define MOTOR_ENABLE_PIN 5
+
+#define MOTOR_DIR_PORT PORTB
+#define MOTOR_DIR_PIN 3
+
+
+#define BUTTON1_PORT PORTB
+#define BUTTON1_PIN 6
+
+#define BUTTON2_PORT PORTB
+#define BUTTON2_PIN 7
+
+
+/*******************************/
+void set_dir(uint8_t dir)
+{
+    if(dir==0){
+        sbi(MOTOR_DIR_PORT, MOTOR_DIR_PIN );
+    }else{
+        cbi(MOTOR_DIR_PORT, MOTOR_DIR_PIN );
+    }
+
+}
+
+/*******************************/
+
+void gen_pulses(uint16_t num, uint16_t del)
+{
+    uint8_t usedelay = 1;
+
+    for(uint16_t c=0;c<num;c++)
+    {
+        sbi(MOTOR_ENABLE_PORT, MOTOR_ENABLE_PIN );
+        if(usedelay) _delay_us(del);
+
+        cbi(MOTOR_ENABLE_PORT, MOTOR_ENABLE_PIN );
+        if(usedelay) _delay_us(del); 
+    } 
+  
+
+    //MOTOR_ENABLE_PORT ^= 0xff;
+    //MOTOR_ENABLE_PORT ^=  0xff; 
+
+}
+
+
+
+
+/*******************************/
+int main (void)
+{
+   
+    //DDRB =0b00001000;  //arduino pin 11 
+    //DDRB |= (1 << 3);  //arduino pin 11
+
+    //DDRD = 0xff;        
+    //DDRD |= (0x4);  
+    
+
+    DDRB = 0xff;
+
+
+
+    while (1)
+    {
+        // set_dir(0);         
+        // gen_pulses(3000, 100);
+        // set_dir(1);         
+        // gen_pulses(3000, 100);
+
+
+        set_dir(0);         
+        gen_pulses(10000, 50);
+        set_dir(1);         
+        gen_pulses(10000, 50);
+
+
+    }
+
+} 
+
+/*******************************/
+
+
+
